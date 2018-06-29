210ee0a89d02b91031d568542578b347ba708a5d
Joris De Winne <joris.dewinne@gmail.com>
2018-06-29 11:02:45 -0700

    Fix typo with wildfly version

--- travis-ci.sh
+++ travis-ci.sh
@@ -2,6 +2,6 @@
 
 image_name="xebialabsunsupported/xl-docker-demo-jboss"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-tag=9.0.2.Final
+tag="9.0.2.Final"
 docker build -t $image_name:$tag --build-arg wildfly_version=$tag .
 docker push $image_name:$tag
